e99c7b6d5cab19545bf40c549a6bac6e9d799429
diksha195 <63047317+diksha195@users.noreply.github.com>
2022-04-09 23:11:46 +0530

    Add files via upload

---
+++ admin/connection.inc.php
@@ -0,0 +1,15 @@
+<?php
+$con=mysqli_connect("localhost","root","","ecom");
+/*
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/ProjectINT220/');
+define('SITE_PATH','localhost/ProjectINT220/');
+
+define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
+define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
+
+define('APP_NAME',"beta");  
+define('HTTP_SERVER', 'http://localhost/'); 
+define('SITE_NAME', 'http://localhost/');   
+define('DOCUMENT_ROOT',$_SERVER['DOCUMENT_ROOT'].APP_NAME); 
+'.HTTP_SERVER.'*/
+?>
